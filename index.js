837b4cb8ce590bec936a5757648cf2d7afe1f7c7
Andrew Spadaro <andrewspadaro@gmail.com>
2020-12-01 16:26:03 -0500

    everthing is working just need to make video and add it to the readme

--- index.js
+++ index.js
@@ -5,41 +5,73 @@ const fs = require('fs')
 const path = require('path')
 
 const questions = [
-    {
-        type: "input",
-        name: "title",
-        message: "What is the title?"
-    },
-    {
-        type: "input",
-        name: "description",
-        message: "Enter a short description about your project"
-    }   
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a short description about your project"
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "enter instilation instructions"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "enter usage instructions"
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "enter contribution instructions"
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "testing instructions"
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "choose a license",
+    choices: ["NONE", "MIT", "APACHE 2.0"]
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "what is your github username"
+  },
+
+
+
+
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-      });
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
-
 // function to initialize program
 function init() {
-    inquirer.prompt(questions)
-      .then(function(answers) {
-     // console.log(JSON.stringify(answers, null, 2));
-     generateMarkdown(answers)
-      })
-      .catch(error => {
-        if(error.isTtyError) {
-          // Prompt couldn't be rendered in the current environment
-        } else {
-          // Something else when wrong
-        }
-      });
+  inquirer.prompt(questions)
+    .then(function (answers) {
+      // console.log(JSON.stringify(answers, null, 2));
+
+      writeToFile('README.md', generateMarkdown({...answers}))
+    })
+    .catch(error => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else when wrong
+      }
+    });
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,45 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return console.log(`# ${data.title}
+  return `
+  # ${data.title}
 
-`);
+  ## Description
+    ${data.description}
+
+  ### Table Of Contents
+
+  1. [Intsallation](#installation)
+  
+  2. [Usage](#usage)
+
+  3. [Contributing](#contributing)
+
+  4. [Tests](#tests)
+
+  5. [License](#license)
+
+  6. [Github](#github)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## License
+  ${data.license}
+
+  ## Github Username
+  ${data.github}
+
+
+`;
 }
 
 module.exports = generateMarkdown;
